8aacaa99063e37e3f1f17f9957dc98322f416ff3
Nurlybek316728 <utebekaigerim@gmail.com>
2022-11-21 04:43:19 +0600

    order

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
 const Order = () => {
-  return (
-    <div>Order</div>
-  )
+    return (
+        <div>
+            <div className="universial_text">
+                Заказы
+            </div>
+            <div className="container">
+                <div className="data__time ">
+                    <div className="row">
+                        <div className="from__time  col-6">
+                            <div className="uni__text-">
+                                Заказы от даты
+                            </div>
+                            <div className="data__input">
+                                <input type='date'  placeholder=' asdsad'></input>
+                            </div>
+                        </div>
+                        <div className="to__time col-6">
+                        <div className="uni__text-">
+                                Заказы от даты
+                            </div>
+                            <div className="data__input">
+                                <input type='date'  placeholder=' asdsad'></input>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Order
